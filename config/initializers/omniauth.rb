566a7d94c4ccac0234c93e66f26baaa11b158003
mmplisskin <mmplisskin@gmail.com>
2015-07-23 19:26:24 -0700

    configures google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"]
-	# provider :google_oauth2, ENV["google_client_id"], ENV["google_secret"],
-  #        scope: 'email', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
+	provider :google_oauth2, ENV["google_client_id"], ENV["google_secret"],
+	         scope: 'email', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
 end
