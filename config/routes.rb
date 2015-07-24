dde7968aae2daec2ddede9b5baa78bfa4dddbb7c
mmplisskin <mmplisskin@gmail.com>
2015-07-23 19:00:37 -0700

    adds media queries and auth for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -32,8 +32,10 @@ Rails.application.routes.draw do
       end
     end
 
-    require 'sidekiq/web'
-    mount Sidekiq::Web => '/bitcoinsrcool'
-
+    require "sidekiq/web"
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+    end if Rails.env.production?
+    mount Sidekiq::Web, at: "/sidekiq"
 
 end
